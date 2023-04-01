87c0b4617f69ccfc5a019402b27864ab75a4181c
Tyler Rudman <tyler.rudman@gmail.com>
2023-04-01 18:47:24 -0400

    debugged error codes

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,26 +1,26 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('./models');
-const { signToken } = require('./utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+        const userData = await User.findOne({ _id: context.user._id }).select("-__v -password");
         return userData;
       }
-      throw new AuthenticationError('Not logged in');
+      throw new AuthenticationError("Not logged in");
     },
   },
   Mutation: {
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
       if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
       const correctPw = await user.isCorrectPassword(password);
       if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
       const token = signToken(user);
       return { token, user };
@@ -36,10 +36,10 @@ const resolvers = {
           { _id: context.user._id },
           { $addToSet: { savedBooks: input } },
           { new: true }
-        ).populate('savedBooks');
+        ).populate("savedBooks");
         return updatedUser;
       }
-      throw new AuthenticationError('You need to be logged in!');
+      throw new AuthenticationError("You need to be logged in!");
     },
     removeBook: async (parent, { bookId }, context) => {
       if (context.user) {
@@ -47,10 +47,10 @@ const resolvers = {
           { _id: context.user._id },
           { $pull: { savedBooks: { bookId } } },
           { new: true }
-        ).populate('savedBooks');
+        ).populate("savedBooks");
         return updatedUser;
       }
-      throw new AuthenticationError('You need to be logged in!');
+      throw new AuthenticationError("You need to be logged in!");
     },
   },
 };
